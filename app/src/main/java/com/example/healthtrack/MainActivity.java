011833bcb00350e01726adcc98b525e22ec2c33b
Clément <clementriou2a@gmail.com>
2024-02-21 21:30:08 +0100

    Fixed bottomNavigationView

--- app/src/main/java/com/example/healthtrack/MainActivity.java
+++ app/src/main/java/com/example/healthtrack/MainActivity.java
@@ -2,48 +2,45 @@ package com.example.healthtrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
+import com.example.healthtrack.databinding.ActivityMainBinding;
 import com.example.healthtrack.fragments.HomeFragment;
 import com.example.healthtrack.fragments.LoginFragment;
 import com.example.healthtrack.fragments.RegisterFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
 
-        // Affiche le fragment Home par défaut
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                new HomeFragment()).commit();
+        binding.bottomNavigationView.setOnItemSelectedListener(item->{
+            if (item.getItemId() == R.id.action_home) {
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.action_register) {
+                replaceFragment(new RegisterFragment());
+            } else if (item.getItemId() == R.id.action_login) {
+                replaceFragment(new LoginFragment());
+            }
+            return true;
+        });
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(MenuItem item) {
-                    Fragment selectedFragment = null;
+    protected void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-                    // Sélectionne le fragment en fonction de l'item du menu sélectionné
-                    switch (item.getItemId()) {
+        fragmentTransaction.replace(R.id.fragment_container,fragment);
+        fragmentTransaction.commit();
 
-                    }
+    }
 
-                    // Remplace le fragment actuel par le fragment sélectionné
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            selectedFragment).commit();
-
-                    return true;
-                }
-            };
 }
