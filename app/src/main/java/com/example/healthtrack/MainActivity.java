8d8a88f38386c15043b2a86770704f221ec7c8d3
Clément <clementriou2a@gmail.com>
2024-02-25 23:15:06 +0100

    basic map fragment + changement strings

--- app/src/main/java/com/example/healthtrack/MainActivity.java
+++ app/src/main/java/com/example/healthtrack/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.healthtrack;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -11,9 +12,14 @@ import com.example.healthtrack.databinding.ActivityMainBinding;
 import com.example.healthtrack.fragments.HomeFragment;
 import com.example.healthtrack.fragments.LoginFragment;
 import com.example.healthtrack.fragments.RegisterFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +35,20 @@ public class MainActivity extends AppCompatActivity {
                 replaceFragment(new RegisterFragment());
             } else if (item.getItemId() == R.id.action_login) {
                 replaceFragment(new LoginFragment());
+            }else if (item.getItemId() == R.id.action_map) {
+                replaceMapFragment();
             }
             return true;
         });
+    }
+
+    private void replaceMapFragment() {
+        SupportMapFragment mapFragment = SupportMapFragment.newInstance();
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, mapFragment)
+                .commit();
     }
 
     protected void replaceFragment(Fragment fragment){
@@ -43,4 +60,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng location = new LatLng(47.5872965,1.3338788);
+        googleMap.addMarker(new MarkerOptions().position(location).title("Blois"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,12));
+    }
 }
